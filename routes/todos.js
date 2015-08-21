59b4fc74fd185eb80572aea41a6bfa07668ad2ab
bastien <bastien.guichard@gmail.com>
2015-08-21 09:14:46 +0200

    01: firts commit on debian

---
+++ routes/todos.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing.
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+*/
+
+var mongoose = require('mongoose');
+var Todo = require('../models/Todo.js');
+
+
+
+// CRUD WITH MONGOOSE
+
+// List: GET /todos
+/* GET /todos listing. */
+router.get('/', function(req, res, next) {
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+/* POST /todos */
+// Create: POST /todos
+router.post('/', function(req, res, next) {
+  Todo.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /todos/id */
+// Show: GET /todos/:id
+router.get('/:id', function(req, res, next) {
+  Todo.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Update: PUT /todos/:id
+/* PUT /todos/:id */
+router.put('/:id', function(req, res, next) {
+  Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Destroy: DELETE /todos/:id
+/* DELETE /todos/:id */
+router.delete('/:id', function(req, res, next) {
+  Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+module.exports = router;
